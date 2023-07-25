10ebfe6596c807d170671b1d043ae0e31c8c25f1
Stefan Neidig <stefan@rpdev.net>
2023-07-25 20:04:46 +0200

    chore: add feedback button to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,16 @@
 import { version } from '../../package.json';
 
 export const Footer = () => {
-  const showFeedback = false;
+  const showFeedback = true;
+  const navigateToUrl = () => {
+    window.open('https://github.com/dasheck0/miro-sequencer/issues', '_blank');
+  };
 
   return (
     <div className='grid' style={{ width: '100%' }}>
       {showFeedback && (
         <div className='cs1 ce4'>
-          <a href='mailto:developers@nanogiants.de'>Feedback</a>
+          <button className='button-icon button-icon-small icon-comment-feedback' type='button' onClick={navigateToUrl}></button>
         </div>
       )}
       <div className='cs8 ce12' style={{ textAlign: 'right', color: '#b2b2b2', fontSize: '0.8rem' }}>
